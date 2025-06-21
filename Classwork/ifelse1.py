8da28d634153065e76d02abfbda7dc1365d16e40
kamini <kaminipadiya14@gmail.com>
2025-06-21 09:06:28 +0530

    ifelse

---
+++ Classwork/ifelse1.py
@@ -0,0 +1,11 @@
+marks = int(input("Enter Marks :"))
+if marks>91 and marks<=100 :
+    print("a grade")
+elif marks>71 and marks<=90 :
+    print("b grade")
+elif marks>51 and marks<=70 :
+    print("c grade")
+elif marks>36 and marks<=50 :
+    print("d grade")
+else :
+    print("invalid")
